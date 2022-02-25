3f2a75148fd40ca74116834810d5540d1149fc34
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-26 00:49:35 +0900

    HOME.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import BannerImage from '../assets/spaghetti-g4b4b17572_1920.jpg';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Hello world</div>
+        <div className='home'>
+          <div className='headerContainer' style={{ backgroundImage: `url(${BannerImage})` }}>
+            <h1> Pasta</h1>
+            <p>Pasta To FIT ANY TAST</p>
+            <Link to="/menu">
+            <button> ORDER NOW</button>
+            </Link>
+          </div>
+        </div>
   );
 }
 
