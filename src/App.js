c8994d2e2b2f11baa8f2b2cb1001871825ae2e5b
hideaking2021 <hide.freetimes.2021@gmail.com>
2022-02-26 14:46:22 +0900

    レイアウト

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+
 
 function App() {
   return (
@@ -11,6 +16,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Switch>
         <Footer />
       </Router>
--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -1,7 +1,39 @@
-export const MenuTitle = [
+import Pepperoni from "../assets/spaghetti-g745512e59_1920.jpg";
+import Margherita from "../assets/pasta-gc307a9b16_1920.jpg";
+import PedroTechSpecial from "../assets/pasta-g53b5480f5_1920.jpg";
+import Vegan from "../assets/pasta-g809379545_1920.jpg";
+import Pineapple from "../assets/pasta-g357549c37_1920.jpg";
+import Expensive from "../assets/pasta-gd62535844_1920.jpg";
+
+export const MenuList = [
   {
-    name: "Pepperoni Pasta",
-    image: "pepperoni",
-    price: 12.99
-  }
-]
+    name: "shrimp Pasta",
+    image: Pepperoni,
+    price: 15.99,
+  },
+  {
+    name: "Cream Salmon Pasta",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "Bacon Mushroom Pasta",
+    image: PedroTechSpecial,
+    price: 17.53,
+  },
+  {
+    name: "Spinach Tagliatelle Pasta",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Seafood pasta",
+    image: Pineapple,
+    price: 11.99,
+  },
+  {
+    name: "Olive Tomato Pasta",
+    image: Expensive,
+    price: 12.99,
+  },
+];
